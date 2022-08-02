f3fc1100ef7e90ce7827454e46bab1a1ceb2ca9e
RIYAZ AHMAD <riyazahmad.online@gmail.com>
2022-08-02 15:52:18 +0530

    initial

---
+++ syoft_front/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import { Register } from './components/Register'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <Register/>
+    </div>
+  )
+}
+
+export default App
